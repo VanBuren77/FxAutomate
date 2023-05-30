8dab393c2aa3d6f4802cd3a3ef7c0dccd4ef0ab3
Evan <evan.lariviere@gmail.com>
2023-05-30 10:48:53 -0400

    Saving

--- FxAutomate/Program.cs
+++ FxAutomate/Program.cs
@@ -30,70 +30,78 @@ namespace FxAutomate
             excelApp.Visible = false;
             excelApp.DisplayAlerts = false;
             excelApp.EnableSound = false;
-
-            string dir = @"C:\Users\evanl\source\repos\j4j\Sheets";
-            string sourceFile = @"fx.xlsx";
-            string targetFile = @"EX_Rates_FM.xlsm";
-            string targetSheet = "Data";
-
-            var sourceWorkbook = (Workbook)excelApp.Workbooks.Open(dir + "\\" + sourceFile);
-            var sourceWorksheet = (Worksheet)sourceWorkbook.Sheets["fx"];
-
-            var targetWorkbook = (Workbook)excelApp.Workbooks.Open(dir + "\\" + targetFile);
-            var targetWorksheet = (Worksheet)targetWorkbook.Sheets[targetSheet];
-
-            Excel.Range sourceRange = sourceWorksheet.Columns[1];
-            Excel.Range sourceFind = sourceRange.Find(sourceSearchDate);
-            var referenceRow = sourceFind.Row;
-
-            Excel.Range targetRange = targetWorksheet.Columns[1];
-            Excel.Range targetFind = targetRange.Find(What: targetSearchDate, LookIn: Excel.XlFindLookIn.xlValues, LookAt: Excel.XlLookAt.xlWhole);
-            var targetRow = targetFind.Row;
-
-            var templateReferenceRange = (string)targetWorksheet.Range["B" + targetRow.ToString()].Formula;
-            var splitFormula = templateReferenceRange.Split(new string[] { "!B" }, StringSplitOptions.None);
-            var searchText = splitFormula[splitFormula.Length - 1];
-
-            var updateRow = targetWorksheet.Range["B:" + targetRow.ToString() + ":ZZ" + targetRow.ToString()];
-            updateRow.Replace(What: searchText,
-                                Replacement: referenceRow,
-                                LookAt: Excel.XlLookAt.xlPart,
-                                SearchOrder: Excel.XlSearchOrder.xlByRows,
-                                MatchCase: false,
-                                SearchFormat: false,
-                                ReplaceFormat: false
-                            );
-
-            int sourceMaxRowCount = sourceWorksheet.UsedRange.Rows.Count;
-            var sourceMaxDate = (string)sourceWorksheet.Range["A" + sourceMaxRowCount.ToString()].Value;
-            int offset = sourceMaxRowCount - referenceRow;
-
-            // var fillDownRange = targetWorksheet.Range[targetRow.ToString() + ":" + (targetRow + offset).ToString()];
-            var fillDownRange = targetWorksheet.Range[targetRow.ToString() + ":" + (targetRow + offset).ToString()];
-            fillDownRange.FillDown();
-
-            sourceWorkbook.Close();
-
-            // ============================
-            // Fill Downs ->
-            // ============================
-
-            var sourceMaxDateDt = Convert.ToDateTime(sourceMaxDate);
-            // Daily New - >
-            var weeklyDataSheet = (Worksheet)targetWorkbook.Sheets["WeeklyData"];
-            var dailyNewSheet = (Worksheet)targetWorkbook.Sheets["DailyNew"];
-            var monthlyDataSheet = (Worksheet)targetWorkbook.Sheets["MonthlyData"];
             
-            Utils.FillDownUpToDate(weeklyDataSheet, sourceMaxDateDt);
-            Utils.FillDownUpToDate(dailyNewSheet, sourceMaxDateDt);
-            Utils.FillDownUpToDate(monthlyDataSheet, sourceMaxDateDt);
+            try
+            {
+                string dir = @"C:\Users\evanl\source\repos\j4j\Sheets";
+                string sourceFile = @"fx.xlsx";
+                string targetFile = @"EX_Rates_FM.xlsm";
+                string targetSheet = "Data";
+
+                var sourceWorkbook = (Workbook)excelApp.Workbooks.Open(dir + "\\" + sourceFile);
+                var sourceWorksheet = (Worksheet)sourceWorkbook.Sheets["fx"];
+
+                var targetWorkbook = (Workbook)excelApp.Workbooks.Open(dir + "\\" + targetFile);
+                var targetWorksheet = (Worksheet)targetWorkbook.Sheets[targetSheet];
+
+                Excel.Range sourceRange = sourceWorksheet.Columns[1];
+                Excel.Range sourceFind = sourceRange.Find(sourceSearchDate);
+                var referenceRow = sourceFind.Row;
+
+                Excel.Range targetRange = targetWorksheet.Columns[1];
+                Excel.Range targetFind = targetRange.Find(What: targetSearchDate, LookIn: Excel.XlFindLookIn.xlValues, LookAt: Excel.XlLookAt.xlWhole);
+                var targetRow = targetFind.Row;
+
+                var templateReferenceRange = (string)targetWorksheet.Range["B" + targetRow.ToString()].Formula;
+                var splitFormula = templateReferenceRange.Split(new string[] { "!B" }, StringSplitOptions.None);
+                var searchText = splitFormula[splitFormula.Length - 1];
+
+                var updateRow = targetWorksheet.Range["B" + targetRow.ToString() + ":ZZ" + targetRow.ToString()];
+                updateRow.Replace(What: searchText,
+                                    Replacement: referenceRow,
+                                    LookAt: Excel.XlLookAt.xlPart,
+                                    SearchOrder: Excel.XlSearchOrder.xlByRows,
+                                    MatchCase: false,
+                                    SearchFormat: false,
+                                    ReplaceFormat: false
+                                );
+
+                int sourceMaxRowCount = sourceWorksheet.UsedRange.Rows.Count;
+                var sourceMaxDate = (string)sourceWorksheet.Range["A" + sourceMaxRowCount.ToString()].Value;
+                int offset = sourceMaxRowCount - referenceRow;
+
+                // var fillDownRange = targetWorksheet.Range[targetRow.ToString() + ":" + (targetRow + offset).ToString()];
+                var fillDownRange = targetWorksheet.Range[targetRow.ToString() + ":" + (targetRow + offset).ToString()];
+                fillDownRange.FillDown();
+
+                sourceWorkbook.Close();
+
+                // ============================
+                // Fill Downs ->
+                // ============================
+
+                var sourceMaxDateDt = Convert.ToDateTime(sourceMaxDate);
+                // Daily New - >
+                var weeklyDataSheet = (Worksheet)targetWorkbook.Sheets["WeeklyData"];
+                var dailyNewSheet = (Worksheet)targetWorkbook.Sheets["DailyNew"];
+                var monthlyDataSheet = (Worksheet)targetWorkbook.Sheets["MonthlyData"];
+
+                Utils.FillDownUpToDate(weeklyDataSheet, sourceMaxDateDt);
+                Utils.FillDownUpToDate(dailyNewSheet, sourceMaxDateDt);
+                Utils.FillDownUpToDate(monthlyDataSheet, sourceMaxDateDt);
 
 
-            // Save it and close.
-            targetWorkbook.SaveAs(dir + "\\" + targetFile.Replace(".xlsm", "_modified.xlsm"));
-            targetWorkbook.Close();
-            excelApp.Quit();
-            Marshal.ReleaseComObject(excelApp);
+                // Save it and close.
+                targetWorkbook.SaveAs(dir + "\\" + targetFile.Replace(".xlsm", "_modified.xlsm"));
+                targetWorkbook.Close();
+                excelApp.Quit();
+                Marshal.ReleaseComObject(excelApp);
+            }
+            catch
+            {
+                excelApp.Quit();
+                Marshal.ReleaseComObject(excelApp);
+            }
         }
 
 
